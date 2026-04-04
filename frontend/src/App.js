e6d2766571d1192081cff8b52b6fa6efb3839e06
aslam1105 <aslam3117@gmail.com>
2026-04-04 23:28:00 +0530

    Frontend and server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
     }
   };
 
+  
   // ================= HANDLE SUBMIT =================
   const handleSubmit = async (e) => {
     e.preventDefault();
--- server/models/Blog.js
+++ server/models/Blog.js
@@ -10,4 +10,5 @@ const BlogSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+
 module.exports = mongoose.model("Blog", BlogSchema);
